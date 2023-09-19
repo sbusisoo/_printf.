8980055c9e53bbda466f778680915c12b9ba6da3
Kwandaa <nokwandaphewa18@gmail.com>
2023-09-19 10:36:31 -0700

    updated get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * get_precision - Calculates the precision for printing
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * get_precision - Calculate precision for printing.
+ * @format: Formatted string in which arguments will be printed.
+ * @i: List arguments to be printed.
+ * @list: List of arguments
  *
- * Return: Precision.
+ * Return: Precision
  */
 int get_precision(const char *format, int *i, va_list list)
 {
