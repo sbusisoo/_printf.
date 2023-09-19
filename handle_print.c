c758cffe29ab4f9d43114ef13b191a0ef47ad5aa
sbusisoo <mshenguussbusiso@gmail.com>
2023-09-19 01:50:31 -0700

    edited

--- handle_print.c
+++ handle_print.c
@@ -1,20 +1,21 @@
 #include "main.h"
 /**
- * handle_print - Prints an argument based on its type
- * @fmt: Formatted string in which to print the arguments.
- * @list: List of arguments to be printed.
- * @ind: ind.
- * @buffer: Buffer array to handle print.
+ * handle_print - a function that prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments
+ * @list: Lists of arguments to be printed
+ * @ind: ind
+ * @buffer: Buffer array to handle print
  * @flags: Calculates active flags
- * @width: get width.
+ * @width: gets the width
  * @precision: Precision specification
- * @size: Size specifier
+ * @size: the size specifier
  * Return: 1 or 2;
  */
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int i, unknow_len = 0, printed_chars = -1;
+	int n, unknow_len = 0, printed_chars = -1;
+
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -22,11 +23,11 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-	for (i = 0; fmt_types[i].fmt != '\0'; i++)
-		if (fmt[*ind] == fmt_types[i].fmt)
-			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
+	for (n = 0; fmt_types[n].fmt != '\0'; i++)
+		if (fmt[*ind] == fmt_types[n].fmt)
+			return (fmt_types[n].fn(list, buffer, flags, width, precision, size));
 
-	if (fmt_types[i].fmt == '\0')
+	if (fmt_types[n].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
--- utils.c
+++ utils.c
@@ -3,7 +3,6 @@
 /**
  * is_printable - Evaluates if a char is printable
  * @c: Char to be evaluated.
- *
  * Return: 1 if c is printable, 0 otherwise
  */
 int is_printable(char c)
@@ -38,9 +37,8 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 }
 
 /**
- * is_digit - Verifies if a char is a digit
- * @c: Char to be evaluated
- *
+ * is_digit - a function that verifies if a character is a digitir not
+ * @c: Character to be evaluated
  * Return: 1 if c is a digit, 0 otherwise
  */
 int is_digit(char c)
@@ -52,10 +50,9 @@ int is_digit(char c)
 }
 
 /**
- * convert_size_number - Casts a number to the specified size
- * @num: Number to be casted.
- * @size: Number indicating the type to be casted.
- *
+ * convert_size_number - a fucntions that casts a number to the specified size
+ * @num: Number to be casted
+ * @size: Number indicating the type to be casted
  * Return: Casted value of num
  */
 long int convert_size_number(long int num, int size)
@@ -69,10 +66,9 @@ long int convert_size_number(long int num, int size)
 }
 
 /**
- * convert_size_unsgnd - Casts a number to the specified size
- * @num: Number to be casted
- * @size: Number indicating the type to be casted
- *
+ * convert_size_unsgnd - a function that casts a number to the specified size
+ * @num: Num to be casted
+ * @size: Num indicating the type to be casted
  * Return: Casted value of num
  */
 long int convert_size_unsgnd(unsigned long int num, int size)
